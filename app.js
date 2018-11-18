2e4131c7e23761cd559732795baf7386571d3866
Pranab <mpranab7890@gmail.com>
2018-11-18 19:53:30 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+var createError = require('http-errors');
+var express = require('express');
+var session = require('express-session')
+var expressValidator = require('express-validator')
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser')
+var mongo = require('mongodb')
+var mongoose = require('mongoose')
+var passport = require('passport')
+var LoaclStrategy = require('passport-local')
+var flash = require('connect-flash');
+var validator = require('validator')
+
+var db = mongoose.connection
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator())
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave:true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash())
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000)
+module.exports = app;
